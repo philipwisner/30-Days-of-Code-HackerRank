5e1279084123f0ed3dc064ea0d0a2df017874b24
philipwisner <philipwisner@gmail.com>
2018-04-24 10:36:21 +0200

    update task

--- Day 4 - Class vs. Instance.js
+++ Day 4 - Class vs. Instance.js
@@ -1,11 +1,9 @@
-DAY 4: Day 4: Class vs. Instance
+//DAY 4: Day 4: Class vs. Instance
 
-Task 
-Write a Person class with an instance variable, , and a constructor that takes an integer, , as a parameter. The constructor must assign  to  after confirming the argument passed as  is not negative; if a negative argument is passed as , the constructor should set  to  and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
-
-yearPasses() should increase the  instance variable by .
-amIOld() should perform the following conditional actions:
-If , print You are young..
-If  and , print You are a teenager..
-Otherwise, print You are old..
-To help you learn by example and complete this challenge, much of the code is provided for you, but you'll be writing everything in the future. The code that creates each instance of your Person class is in the main method. Don't worry if you don't understand it all quite yet!
+//Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge, as a parameter. The constructor must assign initialAge to  age after confirming the argument passed as initialAge is not negative; if a negative argument is passed as initialAge, the constructor should set age to 0 and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+//
+//yearPasses() should increase the  instance variable by 1.
+//amIOld() should perform the following conditional actions:
+//If age < 13, print You are young..
+//If age >= 14 and age < 18, print You are a teenager..
+//Otherwise, print You are old..
