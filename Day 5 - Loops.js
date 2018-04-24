94924a03706894463710ef22feae61fa2e498ae8
philipwisner <philipwisner@gmail.com>
2018-04-24 11:15:27 +0200

    day 5 done

--- Day 5 - Loops.js
+++ Day 5 - Loops.js
@@ -2,3 +2,11 @@
 
 //Given an integer n, print its first 10 multoples. Eacher multiple n x i, where 1 <= i <= 10 should be printed on anew line in the form: n x i = result;
 
+function main() {
+    var n = parseInt(readLine());
+		for (var i = 1; i <= 10; i++) {
+			var result = n * i;
+			var i = i;
+			console.log(n + ' x ' + i + ' = ' + result);
+		}
+}
