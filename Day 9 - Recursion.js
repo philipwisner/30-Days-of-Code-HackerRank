911638955aa66fcc6152f57d48e5d239c2ac5290
philipwisner <philipwisner@gmail.com>
2018-04-27 18:08:58 +0200

    day 9 done

--- Day 9 - Recursion.js
+++ Day 9 - Recursion.js
@@ -1,35 +1,31 @@
 //DAY 9: Recursion
 
-//Given  names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. You will then be given an unknown number of names to query your phone book for. For each name queried, print the associated entry from your phone book on a new line in the form name=phoneNumber; if an entry for name is not found, print Not found instead.
+//rite a factorial function that takes a positive integer, N as a parameter and prints the result of N! (N factorial).
 
-function processData(input) {
-    var inputSplit = input.split('\n');
-    var n = parseInt(inputSplit[0]);
-    var searchLength = inputSplit.length - n;
-    var phoneBook = [];
-    
-    for (var i = 1; i <= n; i++) {
-        var list = inputSplit[i].split(' ');
-        phoneBook[list[0]] = list[1];
-    }
-    
-    for (var i = 1; i < searchLength; i++) {
-        var name = inputSplit[i + n];
-        if (phoneBook.hasOwnProperty(name)) {
-            console.log(name + '=' + phoneBook[name]);
-        } else {
-            console.log('Not found');
-        }
-    }
-} 
+function factorial(n) {
+//Reject number if less than 0; 
+	if (n < 0) {
+		 return -1;
+	 }
+//If number is 0, return 1;
+	else if (n === 0) {
+		return 1;
+	}
+//Perform factorial here
+	else {
+		return (n * factorial(n - 1));
+	}
+	
+};
 
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
-});
+//WITHOUT RECURSION
 
-process.stdin.on("end", function () {
-   processData(_input);
-});
+function factorial2(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+		for (var i = n - 1; i >= 1; i--) {
+			n *= i;
+		}
+		return n;
+	} 
+}
