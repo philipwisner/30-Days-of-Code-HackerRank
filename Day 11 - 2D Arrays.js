e69166052d76537193a65da4a5324031ac64af5e
philipwisner <philipwisner@gmail.com>
2018-04-30 01:37:27 +0200

    done

--- Day 11 - 2D Arrays.js
+++ Day 11 - 2D Arrays.js
@@ -8,11 +8,17 @@ function main() {
        arr[arr_i] = readLine().split(' ');
        arr[arr_i] = arr[arr_i].map(Number);
     }
+
+    var hourglassSumArray = [];
     
     for (var i = 0; i < arr.length; i++) {
         for (var j = 0; j < arr[i].length; j++) {
-            
+            if (arr[i] && arr[i+1] && arr[i+2]) {
+                if (arr[i][j+1] != undefined && arr[i][j+2] != undefined) {
+                    hourglassSumArray.push(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]);
+                }
+            }
         }
     }
-
+    console.log(Math.max.apply(null, hourglassSumArray));
 }
