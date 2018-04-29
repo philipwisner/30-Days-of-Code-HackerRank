43f77b7c27af78c130246b8f5eac5ed0b9e2e331
philipwisner <philipwisner@gmail.com>
2018-04-30 00:14:18 +0200

    day 11

---
+++ Day 11 - 2D Arrays.js
@@ -0,0 +1,18 @@
+//DAY 11: 2D Arrays
+
+//Sum up the values of an hourglass shape in a 2D array
+
+function main() {
+    var arr = [];
+    for(arr_i = 0; arr_i < 6; arr_i++){
+       arr[arr_i] = readLine().split(' ');
+       arr[arr_i] = arr[arr_i].map(Number);
+    }
+    
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            
+        }
+    }
+
+}
