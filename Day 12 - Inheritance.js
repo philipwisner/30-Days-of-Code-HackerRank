9b826d0725df07cada8c22b789ba8a0be99c271a
philipwisner <philipwisner@gmail.com>
2018-04-30 22:09:06 +0200

    day 12 start

---
+++ Day 12 - Inheritance.js
@@ -0,0 +1,47 @@
+DAY 12: Inheritance
+
+/* You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+A Student class constructor, which has  parameters:
+A string, firstName.
+A string, lastName.
+An integer, if.
+An integer array (or vector) of test scores, scores.
+A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average: 
+*/
+
+class Person {
+    constructor(firstName, lastName, identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+    
+    printPerson() {
+        console.log(
+            "Name: " + this.lastName + ", " + this.firstName 
+            + "\nID: " + this.idNumber
+        )
+    }
+}
+
+class Student extends Person {
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+    
+}
