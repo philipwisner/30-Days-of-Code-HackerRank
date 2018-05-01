53e1de7874c3d1b14bcc4bc6447cd2a29c408127
philipwisner <philipwisner@gmail.com>
2018-05-01 23:45:29 +0200

    update

--- Day 12 - Inheritance.js
+++ Day 12 - Inheritance.js
@@ -1,4 +1,4 @@
-DAY 12: Inheritance
+//DAY 12: Inheritance
 
 /* You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
 
