88ac13e950d374942066999065e5cd1671911e06
philipwisner <philipwisner@gmail.com>
2018-05-02 20:24:55 +0200

    day 14

---
+++ Day 14 - Scope.py
@@ -0,0 +1,11 @@
+#DAY 14: Scope
+
+#A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
+#A computeDifference method that finds the maximum absolute difference between any numbers in N and stores it in the maximumDifference instance variable.
+
+#This is not available in JavaScript
+
+
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
