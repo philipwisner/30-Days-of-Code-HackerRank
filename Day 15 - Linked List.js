e3caaef11f38891351d1732f016da1c898414ceb
philipwisner <philipwisner@gmail.com>
2018-05-03 22:30:08 +0200

    day 15

---
+++ Day 15 - Linked List.js
@@ -0,0 +1,3 @@
+DAY 15: Linked List
+
+Complete the insert function in your editor so that it creates a new Node (pass data as the Node constructor argument) and inserts it at the tail of the linked list referenced by the head parameter. Once the new node is added, return the reference to the head node.
