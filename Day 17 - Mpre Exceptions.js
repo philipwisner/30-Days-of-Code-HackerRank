9ada83cf8a73e7e57789b3f008ec2f4fd3ea3730
philipwisner <philipwisner@gmail.com>
2018-05-06 22:10:24 -0400

    day 17 done

--- Day 17 - Mpre Exceptions.js
+++ Day 17 - Mpre Exceptions.js
@@ -1,3 +1,17 @@
 //DAY 17: More Exceptions
 
 //Write a Calculator class with a single method: int power(int,int). The power method takes two integers, n and p, as parameters and returns the integer result of n^p. If either n or p is negative, then the method must throw an exception with the message: n and p should be non-negative.
+
+class Calculator {
+    constructor (n, p) {
+      this.n = n;
+      this.p = p;      
+    }
+    power(n, p) {
+        if (n >= 0 && p >= 0) {
+            return Math.pow(n, p);
+        } else {
+            return 'n and p should be non-negative'
+        }
+    }
+}
