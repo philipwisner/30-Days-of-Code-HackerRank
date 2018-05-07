4e9209db66751f58a1cbe2862bd521a2365876a7
philipwisner <philipwisner@gmail.com>
2018-05-07 12:02:09 -0400

    updated to python

---
+++ Day 19 - Interfaces.py
@@ -0,0 +1,11 @@
+#DAY 19: Interfaces
+
+#The AdvancedArithmetic interface and the method declaration for the abstract divisorSum(n) method are provided for you in the editor below.
+
+#Complete the implementation of Calculator class, which implements the AdvancedArithmetic interface. The implementation for the divisorSum(n) method must return the sum of all divisors of n.
+
+#Not available in JavaScript
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        pass
