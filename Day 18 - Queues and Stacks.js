9d73a9bba2e9a502e66c58d2ce5791ca5e99a1b4
philipwisner <philipwisner@gmail.com>
2018-05-07 11:44:06 -0400

    init day 18

---
+++ Day 18 - Queues and Stacks.js
@@ -0,0 +1,31 @@
+//DAY 18: Queues and Stacks
+
+/*
+To solve this challenge, we must first take each character in s, enqueue it in a queue, and also push that same character onto a stack. Once that's done, we must dequeue the first character from the queue and pop the top character off the stack, then compare the two characters to see if they are the same; as long as the characters match, we continue dequeueing, popping, and comparing each character until our containers are empty (a non-match means s isn't a palindrome).
+
+Write the following declarations and implementations:
+1. Two instance variables: one stack for your queue, and one for your .
+2. A void pushCharacter(char ch) method that pushes a character onto a stack.
+3. A void enqueueCharacter(char ch) method that enqueues a character in the queue instance variable.
+4. A char popCharacter() method that pops and returns the character at the top of the stack instance variable.
+5. A char dequeueCharacter() method that dequeues and returns the first character in the queue instance variable.
+*/
+
+function Solution(s){
+    this.string = s;
+    this.queue = queue;
+    this.stack = stack;
+    
+    this.pushCharacter = function() {
+        
+    };
+    this.enqueueCharacter = function() {
+        
+    };
+    this.popCharacter = function() {
+        
+    };
+    this.dequeueCharacter = function() {
+        
+    };
+}
