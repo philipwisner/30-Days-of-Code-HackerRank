a97bd4ea833f536cc880914ab254abf81aa1e676
philipwisner <philipwisner@gmail.com>
2018-05-09 13:12:28 -0400

    day 21

--- Day 21 - Generics.java
+++ Day 21 - Generics.java
@@ -1,8 +1,12 @@
-//DAY 20: Sorting
+//DAY 21: Generics
 
 /*
-Using a bubble sort return how many swaps were completed in order to return the array in numerical order. Also return the first Element and the last element in the array.
+Write a single generic function named printArray; this function must take an array of generic elements as a parameter (the exception to this is C++, which takes a vector). The locked Solution class in your editor tests your function.
+
+Note: You must use generics to solve this challenge. Do not write overloaded functions.
 */
+
+//Not available in JavaScript or Python
 
 function main() {
     var n = parseInt(readLine());
