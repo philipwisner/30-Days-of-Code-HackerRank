a84cbacecadf10cf4088d5378dafe319e71b7d21
philipwisner <philipwisner@gmail.com>
2018-05-12 18:09:47 -0400

    day 24 done

--- Day 24 - More Linked Lists.js
+++ Day 24 - More Linked Lists.js
@@ -33,7 +33,16 @@ function Node(data) {
 }
 function Solution() {
     this.removeDuplicates = function (head) {
-        //Write your code here
+        if (head == null || head.next == null) {
+            return head;
+        }
+        if (head.data == head.next.data) {
+            head.next = head.next.next;
+            this.removeDuplicates(head);
+        } else {
+            this.removeDuplicates(head.next);
+        }
+        return head;
     }
 
 
