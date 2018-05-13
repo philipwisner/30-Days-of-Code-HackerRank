7b79bce37f4274ffb510da312c436fbcd159d80d
philipwisner <philipwisner@gmail.com>
2018-05-13 15:01:18 -0400

    day 25

---
+++ Day 25 - Running Time and Complexity.py
@@ -0,0 +1,5 @@
+# DAY 25: Running Time and Complexity
+
+# A prime is a natural number greater than 1 that has no positive
+# divisors other than 1 and itself. Given a number, n, determine 
+# and print whether it's prime or notPrime.
