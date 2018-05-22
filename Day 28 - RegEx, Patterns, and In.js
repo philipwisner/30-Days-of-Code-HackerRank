fd6e808416f38541d3bb87315b206b2ef75304b4
Philip Wisner <philipwisner@gmail.com>
2018-05-21 22:38:49 -0400

    added inputs

--- Day 28 - RegEx, Patterns, and In.js
+++ Day 28 - RegEx, Patterns, and In.js
@@ -1,2 +1,2 @@
 // Consider a database table, Emails, which has the attributes First Name and Email ID.
-Given N rows of data simulating the Emails table, print an alphabetically - ordered list of people whose email address ends in @gmail.com.
+// Given N rows of data simulating the Emails table, print an alphabetically - ordered list of people whose email address ends in @gmail.com.
