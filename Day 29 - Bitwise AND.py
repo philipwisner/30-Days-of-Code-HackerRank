547eb6b25ac4ac9a69816604aeae0d129ba1d7bb
Philip Wisner <philipwisner@gmail.com>
2018-06-02 13:51:09 -0400

    day 29 done

---
+++ Day 29 - Bitwise AND.py
@@ -0,0 +1,26 @@
+# Given set S = {1,2,3,...,N}.Find two integers, A and B (where A < B), from set S such that the value of A&B is the maximum possible and also less than a given integer, K. In this case, & represents the bitwise AND operator.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+T = int(input().strip())
+for _ in range(T):
+    n , k = map(int , input().split())
+    print(k-1 if ((k-1) | k) <= n else k-2)
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
